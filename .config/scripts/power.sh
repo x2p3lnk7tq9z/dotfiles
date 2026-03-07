24797d719976aae171db7e767caafe9ce4f996a0
silly <u65487754@gmail.com>
2026-03-07 14:44:51 +0100

    Update power.sh

--- .config/scripts/power.sh
+++ .config/scripts/power.sh
@@ -1,1 +1,19 @@
+#!/bin/bash
 
+options="lock\nlogout\nshutdown\nreboot"
+choice=$(echo -e "$options" | rofi -dmenu -p "power" -i)
+
+case "$choice" in
+    lock)
+        hyprlock
+        ;;
+    logout)
+        command -v hyprshutdown >/dev/null 2>&1 && hyprshutdown || hyprctl dispatch exit
+        ;;
+    shutdown)
+        shutdown now
+        ;;
+    reboot)
+        reboot
+        ;;
+esac
