6bcc1709cf84d566170263cc768edbefeb52071d
silly <u65487754@gmail.com>
2026-03-08 12:46:02 +0100

    Update settings.sh

--- .config/scripts/settings.sh
+++ .config/scripts/settings.sh
@@ -3,7 +3,7 @@
 HYPR_CONF="$HOME/.config/hypr/hyprland.conf"
 DUNST_CONF="$HOME/.config/dunst/dunstrc"
 
-options="sharp/round toggle\nborder toggle"
+options="sharp/round toggle\nborder toggle\ngaps toggle"
 choice=$(echo -e "$options" | fuzzel -d -p "> ")
 
 [[ -z "$choice" ]] && exit
@@ -13,30 +13,17 @@ case "$choice" in
         if grep -q "rounding = 0 # @dynamic_rounding" "$HYPR_CONF"; then
             sed -i '/@dynamic_rounding/c\    rounding = 15 # @dynamic_rounding' "$HYPR_CONF"
             sed -i '/@dynamic_power/c\    rounding_power = 10 # @dynamic_power' "$HYPR_CONF"
-            sed -i '/@dynamic_smartgaps/s/^\([^#]\)/#\1/' "$HYPR_CONF"
             sed -i '/@dynamic_dunst/c\    corner_radius = 15 # @dynamic_dunst' "$DUNST_CONF"
-
             hyprctl keyword decoration:rounding 15 > /dev/null
-            hyprctl keyword decoration:rounding_power 10 > /dev/null
-            hyprctl reload > /dev/null
-
-            killall dunst && dunst &
-            sleep 0.2
             notify-send "toggled" "rounded"
         else
             sed -i '/@dynamic_rounding/c\    rounding = 0 # @dynamic_rounding' "$HYPR_CONF"
             sed -i '/@dynamic_power/c\    rounding_power = 0 # @dynamic_power' "$HYPR_CONF"
-            sed -i '/@dynamic_smartgaps/s/^#//g' "$HYPR_CONF"
             sed -i '/@dynamic_dunst/c\    corner_radius = 0 # @dynamic_dunst' "$DUNST_CONF"
-
             hyprctl keyword decoration:rounding 0 > /dev/null
-            hyprctl keyword decoration:rounding_power 0 > /dev/null
-            hyprctl reload > /dev/null
-
-            killall dunst && dunst &
-            sleep 0.1
             notify-send "toggled" "sharp"
         fi
+        killall dunst && dunst &
         ;;
 
     *border*)
@@ -48,6 +35,22 @@ case "$choice" in
             sed -i '/@dynamic_border/c\    border_size = 0 # @dynamic_border' "$HYPR_CONF"
             hyprctl keyword general:border_size 0 > /dev/null
             notify-send "borders" "disabled"
+        fi
+        ;;
+
+    *gaps*)
+        if grep -q "gaps_in = 0 # @dynamic_gaps_in" "$HYPR_CONF"; then
+            sed -i '/@dynamic_gaps_in/c\    gaps_in = 5 # @dynamic_gaps_in' "$HYPR_CONF"
+            sed -i '/@dynamic_gaps_out/c\    gaps_out = 10 # @dynamic_gaps_out' "$HYPR_CONF"
+            hyprctl keyword general:gaps_in 5 > /dev/null
+            hyprctl keyword general:gaps_out 10 > /dev/null
+            notify-send "gaps" "on"
+        else
+            sed -i '/@dynamic_gaps_in/c\    gaps_in = 0 # @dynamic_gaps_in' "$HYPR_CONF"
+            sed -i '/@dynamic_gaps_out/c\    gaps_out = 0 # @dynamic_gaps_out' "$HYPR_CONF"
+            hyprctl keyword general:gaps_in 0 > /dev/null
+            hyprctl keyword general:gaps_out 0 > /dev/null
+            notify-send "gaps" "off"
         fi
         ;;
 esac
