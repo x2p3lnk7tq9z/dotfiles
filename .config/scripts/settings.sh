37fc73fdc173ebbfca9866c835b9fb64731bf367
silly <u65487754@gmail.com>
2026-03-08 02:31:01 +0100

    Update settings.sh

--- .config/scripts/settings.sh
+++ .config/scripts/settings.sh
@@ -2,13 +2,13 @@
 HYPR_CONF="$HOME/.config/hypr/hyprland.conf"
 DUNST_CONF="$HOME/.config/dunst/dunstrc"
 
-options="󰃟 sharp/round toggle"
+options="󰃟 sharp/round toggle\n󰃇 border toggle"
 choice=$(echo -e "$options" | rofi -dmenu -i -p "settings")
 
 [[ -z "$choice" ]] && exit
 
 case "$choice" in
-    *toggle*)
+    *sharp/round*)
         if grep -q "rounding = 0 # @dynamic_rounding" "$HYPR_CONF"; then
             sed -i '/@dynamic_rounding/c\    rounding = 15 # @dynamic_rounding' "$HYPR_CONF"
             sed -i '/@dynamic_power/c\    rounding_power = 10 # @dynamic_power' "$HYPR_CONF"
@@ -35,6 +35,18 @@ case "$choice" in
             killall dunst && dunst &
             sleep 0.1
             notify-send "toggled" "sharp"
+        fi
+        ;;
+
+    *border*)
+        if grep -q "border_size = 0 # @dynamic_border" "$HYPR_CONF"; then
+            sed -i '/@dynamic_border/c\    border_size = 2 # @dynamic_border' "$HYPR_CONF"
+            hyprctl keyword general:border_size 2 > /dev/null
+            notify-send "borders" "enabled"
+        else
+            sed -i '/@dynamic_border/c\    border_size = 0 # @dynamic_border' "$HYPR_CONF"
+            hyprctl keyword general:border_size 0 > /dev/null
+            notify-send "borders" "disabled"
         fi
         ;;
 esac
