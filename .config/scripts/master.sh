b0cfe786a9a1e1cd42b60bce11422a4350be70f4
silly <u65487754@gmail.com>
2026-03-09 17:07:07 +0100

    Update master.sh

--- .config/scripts/master.sh
+++ .config/scripts/master.sh
@@ -2,12 +2,14 @@
 
 SCRIPT_DIR="$HOME/.config/scripts"
 
-options="wallpaper\nsettings\nfastfetch"
+options="wallpaper\nsettings\nconfig\nbtop\nfastfetch"
 
 choice=$(echo -e "$options" | fuzzel -d -p "> ")
 
 case "$choice" in
     *wallpaper*) "$SCRIPT_DIR/wallpaper.sh" ;;
     *settings*) "$SCRIPT_DIR/settings.sh" ;;
+    *config*) "$SCRIPT_DIR/config.sh" ;;
+    *btop*) kitty btop ;;
     *fastfetch*) kitty sh -c "fastfetch; read -p 'press enter to close...'" ;;
 esac
