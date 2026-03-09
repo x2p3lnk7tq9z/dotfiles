54f60717a3db62ef35d37ee807220eb4b0ffd359
silly <u65487754@gmail.com>
2026-03-09 01:43:38 +0100

    Update wallpaper.sh

--- .config/scripts/wallpaper.sh
+++ .config/scripts/wallpaper.sh
@@ -2,15 +2,21 @@
 
 WALL_DIR="$HOME/.config/wallpaper"
 
-selected=$(ls "$WALL_DIR" | fuzzel -d -p "> ")
+selected=$(echo -e "random\n$(ls "$WALL_DIR")" | fuzzel -d -p "> ")
+
 [[ -z "$selected" ]] && exit
+
+if [ "$selected" = "random" ]; then
+    selected=$(ls "$WALL_DIR" | shuf -n 1)
+fi
+
 FULL_PATH="$WALL_DIR/$selected"
 
 swww img "$FULL_PATH" --transition-type grow --transition-duration 1.5 --transition-fps 120
 
 wal -i "$FULL_PATH" -n -q
 
-kill -SIGUSR1 $(pgrep kitty)
+kill -SIGUSR1 $(pgrep kitty) 2>/dev/null
 
 killall dunst
 dunst &
