b7286f23f28700eb4c59689c1973cf2ef54e82fc
silly <u65487754@gmail.com>
2026-03-15 05:07:46 +0100

    Delete .config/themes/マンガ directory

--- .config/themes/マンガ/config.fish
+++
@@ -1,9 +0,0 @@
-function fish_greeting
-end
-
-abbr -a s sudo
-abbr -a update 'sudo pacman -Syu'
-abbr -a spi 'sudo pacman -S'
-abbr -a yi 'yay -S'
-abbr -a search 'yay -Ss'
-abbr -a fetch fastfetch
