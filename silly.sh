4013badddd10ac90628615ea45d13abff3607331
silly <u65487754@gmail.com>
2026-04-05 06:18:11 +0000

    Update silly.sh

--- silly.sh
+++ silly.sh
@@ -77,6 +77,7 @@ install() {
     xdg-mime default mpv.desktop video/mp4 video/x-matroska video/webm video/quicktime video/x-msvideo video/x-flv
     xdg-mime default dev.zed.Zed.desktop text/plain text/markdown text/x-shellscript text/x-python text/x-lua text/x-perl text/x-ruby text/x-yaml text/css text/html application/json application/xml text/x-csrc text/x-chdr text/x-c++src text/x-rust text/x-go
     gsettings set org.gnome.nautilus.window-state sort-column 'type'
+    printf "file://$HOME/Downloads Downloads\nfile://$HOME/Documents Documents\nfile://$HOME/Pictures Pictures\nfile://$HOME/Videos Videos\nfile://$HOME/.config .config\n" > ~/.config/gtk-3.0/bookmarks
     
     pkill -x firefox && sleep 1 || true
     firefox --headless --no-remote 2>/dev/null &
